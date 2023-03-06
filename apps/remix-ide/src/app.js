aa26a63f8cc260988a5584c4b39f589bad8db6d1
Joseph Izang <josephizang@gmail.com>
2023-03-06 16:23:27 +0100

    update imports

--- apps/remix-ide/src/app.js
+++ apps/remix-ide/src/app.js
@@ -50,7 +50,6 @@ const remixLib = require('@remix-project/remix-lib')
 
 import { QueryParams } from '@remix-project/remix-lib'
 import { SearchPlugin } from './app/tabs/search'
-import { DocGen } from './app/plugins/docgen'
 
 const Storage = remixLib.Storage
 const RemixDProvider = require('./app/files/remixDProvider')
@@ -180,9 +179,6 @@ class AppComponent {
     //----- search
     const search = new SearchPlugin()
 
-    //---- docgen plugin
-    const docgen = new DocGen()
-
     //---------------- Solidity UML Generator -------------------------
     const solidityumlgen = new SolidityUmlGen(appManager)
 
@@ -299,8 +295,7 @@ class AppComponent {
       this.walkthroughService,
       search,
       solidityumlgen,
-      contractFlattener,
-      docgen
+      contractFlattener
     ])
 
     // LAYOUT & SYSTEM VIEWS
